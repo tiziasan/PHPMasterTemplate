37bfed702c96d8778aab830e233ff4c9a0a02a2a
tiziasan <tizianosantilli@gmail.com>
2020-01-12 01:17:12 +0100

    create classes

--- Components/user_profile/user_profile.php
+++ Components/user_profile/user_profile.php
@@ -3,16 +3,42 @@
 
 include("./../../component.php");
 
-$profile = new Component("user_profile.html");
-$profile->set("username", "Tiziobosss");
-$profile->set("name", "Tiziano Santilli");
-$profile->set("age", "23");
-$profile->set("location", "Rieti");
+class user_profile {
+        function show()
+        {
+            $layout = new Component("./../frame/frame.html");
+            echo $layout->output();
+        }
+}
 
-$layout = new Component("./../frame/frame.html");
-$layout->set("title", "User profile");
-$layout->set("content", $profile->output());
+$profile = new user_profile();
+$profile->show();
 
 
-echo $layout->output();
+
+
+
+
+
+
+
+
+
+
+
+
+//$profile = new Component("user_profile.html");
+//$profile->set("username", "Tiziobosss");
+//$profile->set("name", "Tiziano Santilli");
+//$profile->set("age", "23");
+//$profile->set("location", "Rieti");
+
+
+
+//$layout = new Component("./../frame/frame.html");
+//$layout->set("title", "User profile");
+//$layout->set("content", $profile->output());
+
+
+//echo $layout->output();
 
---
+++ init.php
@@ -0,0 +1,4 @@
+<?php
+include ("Components/user_profile/user_profile.php");
+$profile = new user_profile();
+$profile->show();
