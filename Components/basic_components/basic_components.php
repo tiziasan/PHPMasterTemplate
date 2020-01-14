b61873433c9747cb334ed21d65cc6fa2e8b77739
tiziasan <tizianosantilli@gmail.com>
2020-01-14 16:36:07 +0100

    component added

--- Components/basic_components/basic_components.php
+++ Components/basic_components/basic_components.php
@@ -1,43 +1,43 @@
 <?php
-class basic_components{
+class Basic_components{
 
     function setHead($title){
-        $component = "<head><title>".$title."</title><link rel=\"stylesheet\" type=\"text/css\" href=\"Components/basic_components/body_style.css\"></head>";
+        $component = "<head><title>".$title."</title><link rel=\"stylesheet\" type=\"text/css\" href=\"Components/basic_components/style.css\"></head>";
         echo $component;
     }
 
-    function setH1($body,$attributes){
+    function setH1($body,$attributes = null){
         $component = "<h1 ". $attributes .  ">".$body."</h1>";
         echo $component;
     }
 
-    function setH2($body,$attributes){
+    function setH2($body,$attributes = null){
         $component = "<h2 ". $attributes .  ">".$body."</h2>";
         echo $component;
     }
 
-    function setH3($body,$attributes){
+    function setH3($body,$attributes = null){
         $component = "<h3 ". $attributes .  ">".$body."</h3>";
         echo $component;
     }
 
-    function setH4($body,$attributes){
+    function setH4($body,$attributes = null){
         $component = "<h4 ". $attributes .  ">".$body."</h4>";
         echo $component;
     }
 
-    function setH5($body,$attributes){
+    function setH5($body,$attributes = null){
         $component = "<h5 ". $attributes .  ">".$body."</h5>";
         echo $component;
     }
 
-    function setH6($body,$attributes){
+    function setH6($body,$attributes = null){
         $component = "<h6 ". $attributes .  ">".$body."</h6>";
         echo $component;
     }
 
-    function setInput($attributes){
-        $component ="<input".$attributes.">";
+    function setInput($attributes = null){
+        $component ="<input ".$attributes.">";
         echo $component;
     }
 
@@ -45,53 +45,53 @@ class basic_components{
         $component = "<br>";
         echo $component;
     }
-    function setButton($body, $attributes){
+    function setButton($body, $attributes = null){
         $component = "<button ".$attributes.">".$body."</button>";
         echo $component;
 
     }
-    function setImg($attributes){
+    function setImg($attributes = null){
         $component = "<img".$attributes.">";
         echo $component;
 
     }
 
-    function setLabel($body, $attributes){
+    function setLabel($body, $attributes = null){
         $component = "<label ".$attributes.">".$body."</label>";
         echo $component;
     }
 
-    function setAReference($body, $attributes){
+    function setAReference($body, $attributes = null){
         $component = "<a ".$attributes.">".$body."</a>";
         echo $component;
     }
 
-    function setOption($body, $attributes){
+    function setOption($body, $attributes = null){
         $component = "<option ".$attributes.">".$body."</option>";
         echo $component;
     }
 
-    function setParagraph($body, $attributes){
+    function setParagraph($body, $attributes = null){
         $component = "<p ".$attributes.">".$body."</p>";
         echo $component;
     }
 
-    function setQuote($body, $attributes){
+    function setQuote($body, $attributes = null){
         $component = "<q ".$attributes.">".$body."</q>";
         echo $component;
     }
 
-    function setScript($body, $attributes){
+    function setScript($body, $attributes = null){
         $component = "<script ".$attributes.">".$body."</script>";
         echo $component;
     }
 
-    function setTextArea($body, $attributes){
+    function setTextArea($body, $attributes = null){
         $component = "<textarea ".$attributes.">".$body."</textarea>";
         echo $component;
     }
 
-    function setListItem($body, $attributes){
+    function setListItem($body, $attributes = null){
         $component = "<li ".$attributes.">".$body."</li>";
         echo $component;
     }
--- init.php
+++ init.php
@@ -3,14 +3,18 @@ error_reporting(E_ALL);
 ini_set("display_errors","On");
 
 
-include ("/Applications/MAMP/htdocs/PHPMasterTemplate/Components/basic_components/basic_components.php");
+include ("Components/basic_components/basic_components.php");
+include ("Components/calendar/calendar.php");
 //include("/Applications/MAMP/htdocs/PHPMasterTemplate/Components/user_profile/user_profile.php");
 
 
-$basic = new basic_components();
-$basic->setHead("prova");
+$basic = new Basic_components();
+$basic->setHead("prova1");
 $basic->setH1("ciao", "id='prova'");
-$basic->setButton("ciao", null);
-$basic->setH6("ciao", null);
+$basic->setLabel("ciao");
+$basic->setInput("type='checkbox'");
+$basic->setTextArea('ipse lorum...', null);
+$calendar = new Calendar();
+
 
 
