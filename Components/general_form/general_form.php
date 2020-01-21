bcb70d44c2b268b06c7110a8d480c9ca2229e378
fons94 <djpoldowlf@gmail.com>
2020-01-21 19:26:06 +0100

    new general form

--- Components/general_form/general_form.php
+++ Components/general_form/general_form.php
@@ -1,1 +1,20 @@
 <?php
+class general_form{
+    function createGeneralForm($array, $attributes){
+        $component = "<form class=\"form-style-9\" action=".$attributes.">
+                    <ul>";
+    foreach ($array as $val) {
+        $component .= "<li>
+    <input type=\"text\" name=\"field1\" class=\"field-style field-split align-left\" placeholder=" . $val . " /> 
+    </li>";
+    }
+
+        $component.= "<li>
+                        <input type=\"submit\" value=\"SUBMIT\" />
+                      </li>
+                    </ul>
+                    </form>";
+
+        echo $component;
+    }
+}
