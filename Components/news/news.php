5d8d80d81a89431378982917688a105d196dff4d
fons94 <djpoldowlf@gmail.com>
2020-01-21 16:39:20 +0100

    new login form new profile card

--- Components/news/news.php
+++ Components/news/news.php
@@ -9,17 +9,13 @@ class news{
                                 <p class=\"mb-0 small font-weight-medium text-uppercase mb-1 text-muted lts-2px\">
                                 ".$category."
                                 </p>
-
                                 <h1 class=\"ff-serif font-weight-normal text-black card-heading mt-0 mb-1\" style=\"line-height: 1.25;\">
                                 ".$title."
                                 </h1>
-
                                 <p class=\"mb-1\">
                                 ".$description." &hellip;
                                 </p>
-
                             </div>
-
                             <a href=\"#0\" class=\"text-uppercase d-inline-block font-weight-medium lts-2px ml-2 mb-2 text-center styled-link\">
                             Read More
                             </a>
