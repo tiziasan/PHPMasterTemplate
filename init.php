116747420606ddce297a10886e2278ca67910522
fons94 <djpoldowlf@gmail.com>
2020-01-21 14:26:28 +0100

    profile card news card login form

--- init.php
+++ init.php
@@ -6,7 +6,10 @@ ini_set("display_errors","On");
 include "Components/basic_components/basic_components.php";
 //include("/Applications/MAMP/htdocs/PHPMasterTemplate/Components/user_profile/user_profile.php");
 include "Components/recipe_card/recipe_card.php";
-
+include "Components/user_profile/user_profile.php";
+include "Components/login_form/login_form.php";
+include "Components/news/news.php";
+include "Components/profile_card/profile_card.php";
 
 $basic = new Basic_components();
 $basic->setHead("prova1");
@@ -15,12 +18,14 @@ $basic->setHead("prova1");
 //$basic->setInput("type='checkbox'");
 //$basic->setTextArea('ipse lorum...', null);
 
-$card = new recipe_card();
-$card -> showRecipeCard("Spaghetti", "30 minuti", "3 porzioni", "Facile", "Se usi la pancetta non sei Italiano!","Cuciniamo!");
+//$card = new recipe_card();
+//$card -> showRecipeCard("Spaghetti", "30 minuti", "3 porzioni", "Facile", "Se usi la pancetta non sei Italiano!","Cuciniamo!");
 
+//$utente = new login_form();
+//$utente -> login("email", "password");
 
+//$news = new news();
+//$news ->insertNews("titolo1", "viaggi", "prova con testo");
 
-
-
-
-
+$profile = new profile_card();
+$profile ->createProfile("pippo", "baudo");
