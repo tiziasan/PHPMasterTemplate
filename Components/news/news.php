b7fa9149fecbdafeaefe79e06a5bf3a5ea59f8ab
fons94 <djpoldowlf@gmail.com>
2020-01-22 16:01:17 +0100

    new general form

--- Components/news/news.php
+++ Components/news/news.php
@@ -2,9 +2,7 @@
 class news{
     function insertNews($title, $category, $description, $img="https://images.pexels.com/photos/442559/pexels-photo-442559.jpeg?auto=compress&cs=tinysrgb"){
         $component = "<div class=\"my-2 mx-auto p-relative bg-white shadow-1 blue-hover\">
-                            <img src='".$img."' alt=\"Man with backpack\"    
-                            class=\"d-block w-full\">
-
+                            <img src=".$img." alt=\"Man with backpack\" class=\"d-block w-full\">
                             <div class=\"px-2 py-2\">
                                 <p class=\"mb-0 small font-weight-medium text-uppercase mb-1 text-muted lts-2px\">
                                 ".$category."
@@ -13,7 +11,7 @@ class news{
                                 ".$title."
                                 </h1>
                                 <p class=\"mb-1\">
-                                ".$description." &hellip;
+                                ".$description."
                                 </p>
                             </div>
                             <a href=\"#0\" class=\"text-uppercase d-inline-block font-weight-medium lts-2px ml-2 mb-2 text-center styled-link\">
