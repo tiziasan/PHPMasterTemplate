f1efc135e711a6c6422135ff0c4296291b9848cb
AposVinc <apos.vincenzo@gmail.com>
2020-02-18 18:48:33 +0100

    column - eliminazione duplicati css

--- Components/five_column_div/five_column_div.php
+++ Components/five_column_div/five_column_div.php
@@ -2,13 +2,13 @@
 
 class five_column_div{
     function setFiveColumnDiv($object1, $object2, $object3, $object4, $object5){
-        $component = "<div class=\"row\">
-                          <div class=\"column-5\">".$object1."</div>
-                          <div class=\"column-5\">".$object2."</div>
-                          <div class=\"column-5\">".$object3."</div>
-                          <div class=\"column-5\">".$object4."</div>
-                          <div class=\"column-5\">".$object5."</div>
-                      </div>";
+        $component = '<div class="row">
+                          <div class="column column-5">'.$object1.'</div>
+                          <div class="column column-5">'.$object2.'</div>
+                          <div class="column column-5">'.$object3.'</div>
+                          <div class="column column-5">'.$object4.'</div>
+                          <div class="column column-5">'.$object5.'</div>
+                      </div>';
         echo $component;
     }
 }
--- Components/one_column_div/one_column_div.php
+++ Components/one_column_div/one_column_div.php
@@ -3,9 +3,9 @@
 class one_column_div{
 
     function setOneColumnDiv($object1){
-        $component = "<div class=\"row\">
-                          <div class=\"column-1\">".$object1."</div>
-                      </div>";
+        $component = '<div class="row">
+                          <div class="column column-1">'.$object1.'</div>
+                      </div>';
         echo $component;
     }
 }
