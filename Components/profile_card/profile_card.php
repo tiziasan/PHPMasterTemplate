83ed3a76e8127ca2480292a61261f83f6080bfc0
AposVinc <apos.vincenzo@gmail.com>
2020-03-20 13:49:36 +0100

    profile card

--- Components/profile_card/profile_card.php
+++ Components/profile_card/profile_card.php
@@ -1,13 +1,15 @@
 <?php
+
 class profile_card{
+
     function createProfile($name,$role,$where,$btnName,$btnURL,$img="https://elysator.com/wp-content/uploads/blank-profile-picture-973460_1280-e1523978675847.png"){
-        $component = "<div class=\"card-profile\">
-                          <img src=\"".$img."\" style=\"width:100%\">
+        $component = "<div class=\"card text-center\">
+                          <img src=\"".$img."\">
                           <h1>".$name."</h1>
                           <p class=\"title-profile\">".$role."</p>
                           <p class = \"p-profile\">".$where."</p>
-                          <p><button class =\"button-profile\" src='".$btnURL."'>".$btnName."</button></p>
-                        </div>";
+                          <button class =\"button-profile\" src='".$btnURL."'>".$btnName."</button>
+                       </div>";
 
         return $component;
     }
