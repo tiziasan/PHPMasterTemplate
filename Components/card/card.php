d96c05100d9a13ae68ff7bd368fe60318ead8517
AposVinc <apos.vincenzo@gmail.com>
2020-04-08 01:23:40 +0200

    Create card.php

---
+++ Components/card/card.php
@@ -0,0 +1,10 @@
+<?php
+
+class card{
+    function setCard($object){
+        $component = '<div class="card">'.
+                          $object
+                      .'</div>';
+        return $component;
+    }
+}
