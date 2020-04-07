75d5d032b1373fe5fedab5606265ab6be98c754e
AposVinc <apos.vincenzo@gmail.com>
2020-04-08 01:23:12 +0200

    flex a map

--- Components/map/map.php
+++ Components/map/map.php
@@ -3,7 +3,7 @@ class map{
 
 
     function setMap($location) {
-        $component = "<div>
+        $component = "<div style=\"display: flex;\">
                         <iframe class=\"gmap_canvas\"  src=\"https://maps.google.com/maps?q=".$location."&t=&z=13&ie=UTF8&iwloc=&output=embed\">
                             </iframe>
                         </div>";
@@ -12,7 +12,7 @@ class map{
     }
 
     function printMap($location) {
-        $component = "<div>
+        $component = "<div style=\"display: flex;\">
                         <iframe class=\"gmap_canvas\"  src=\"https://maps.google.com/maps?q=".$location."&t=&z=13&ie=UTF8&iwloc=&output=embed\">
                             </iframe>
                         </div>";
